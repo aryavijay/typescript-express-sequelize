7b65bddeddff111c9658a05727989cb4f40d89d2
vj <vijay.kumar@absolute.ag>
2023-05-18 11:20:07 +0530

    removied dist folder

--- dist/middleware/responseHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = exports.successHandler = void 0;
-function successHandler(_req, _response, next) {
-}
-exports.successHandler = successHandler;
-function errorHandler(_error, _req, _response, next) {
-}
-exports.errorHandler = errorHandler;
